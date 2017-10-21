b4cc9dce2286d1535a293fe373b12f0e4b122a2f
pigi5 <ford.hash@gmail.com>
2017-10-21 14:34:33 -0500

    added dependencies

---
+++ front/src/search.js
@@ -0,0 +1,97 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import zip from './util';
+
+class SearchResults extends Component {
+    constructor(props) {
+        super(props);
+        // props.searchQuery is the string passed to the backend call
+        this.state = {};
+        /*
+        axios.get('/aliProduct', {
+                params: {
+                    query: this.props.searchQuery
+                }
+            })
+            .then(function(response) {
+                console.log(response);
+                this.state.aliResults = response['data'];
+                if (typeof this.state.amazonResults !== 'undefined') {
+                    this.state.zippedResults = zip(this.state.aliResults, this.state.amazonResults);
+                }
+            })
+            .catch(function(error) {
+                console.log(error);
+            }
+        );
+        axios.get('/amazonProduct', {
+                params: {
+                    query: this.props.searchQuery
+                }
+            })
+            .then(function(response) {
+                console.log(response);
+                this.state.amazonResults = response['data'];
+                if (typeof this.state.aliResults !== 'undefined') {
+                    this.state.zippedResults = zip(this.state.aliResults, this.state.amazonResults);
+                }
+            })
+            .catch(function(error) {
+                console.log(error);
+            }
+        );
+        */
+        this.state.zippedResults[[{name: 'aliproduct1'}, {name: 'amaproduct1'}], 
+            [{name: 'aliproduct2'}, {name: 'amaproduct2'}], 
+            [{name: 'aliproduct3'}, {name: 'amaproduct3'}]];
+    }
+    
+    createProductCard(product) {
+        if (curVal[0] == null) {
+            return (
+                <div className="card">
+                </div>
+            );
+        } else {
+            return (
+                <div className="card">
+                    //<img className="card-img-top" src="..." alt="Card image cap">
+                    <div className="card-block">
+                        <h4 className="card-title">product.name</h4>
+                        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+                    </div>
+                </div>
+            );
+        }
+    }
+
+    createProductRow(curVal, index, array) {
+        return (
+            <div key={index}>
+                {createProductCard(curVal[0])}
+                <div className="card">
+                    //<img className="card-img-top" src="..." alt="Card image cap">
+                    <div className="card-block">
+                        <h4 className="card-title">Spacer</h4>
+                        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+                    </div>
+                </div>
+                {createProductCard(curVal[1])}
+            </div>
+        );
+    }
+
+    render() {
+        return (
+            {
+                <div className='container'>
+                    <div className="card-deck">
+                        {this.state.zippedResults.map(this.createProductRow)}
+                    </div>
+                </div>
+            }
+        );
+    }
+}
