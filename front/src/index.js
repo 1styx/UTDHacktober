ddbc71d4b5ebd14057e9f947edd1d792648a4fa2
pigi5 <ford.hash@gmail.com>
2017-10-21 14:48:33 -0500

    some routing

--- front/src/index.js
+++ front/src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import SearchResults from './search';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Router>
+        <div>
+            <Route exact path='/' component={App} />
+            <Route exact path='/search' component={SearchResults} />
+        </div>
+    </Router>,
+    document.getElementById('root')
+);
 registerServiceWorker();
