63f66f908c60b3e79f2c9989756efe2b896ed4df
EvWilson <everettasherwilson@gmail.com>
2017-10-21 12:23:34 -0500

    Double push

--- front/src/App.js
+++ front/src/App.js
@@ -6,7 +6,7 @@ const navbarInstance = (
   <Navbar inverse collapseOnSelect>
     <Navbar.Header>
       <Navbar.Brand>
-        <a href="#">React-Bootstrap</a>
+        <a href="#">DropSHipping Comparator</a>
       </Navbar.Brand>
       <Navbar.Toggle />
     </Navbar.Header>
