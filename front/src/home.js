1345a40500e5e6bc5bd4012d90ce9cb610818ed0
EvWilson <everettasherwilson@gmail.com>
2017-10-22 08:29:05 -0500

    working

--- front/src/home.js
+++ front/src/home.js
@@ -17,14 +17,36 @@ export default class Home extends Component {
         .catch(function(error) {
             console.log(error);
         });
+
+        this.listElements = this.listElements.bind(this);
     }
+
+    listElements() {
+        return (
+            this.state.docs.forEach(function(doc) {
+                <button type="button" class="list-group-item list-group-item-action">Dapibus ac facilisis in</button>
+            })
+        );
+    }
+
     render() {
         return (
             <div>
                 <Navbar pageName='Home' parentUrl={this.props.match.url} />
                 <div className='container'>
                     <img src={'https://image.ibb.co/ksvCvm/Mother_Ship.png'}/>
-                    Hello World
+                    <h2>Welcome to The MotherShip</h2>
+
+                    <div class="list-group">
+                        <button type="button" class="list-group-item list-group-item-action">
+                            Cras justo odio
+                        </button>
+                        <button type="button" class="list-group-item list-group-item-action">Dapibus ac facilisis in</button>
+                        <button type="button" class="list-group-item list-group-item-action">Morbi leo risus</button>
+                        <button type="button" class="list-group-item list-group-item-action">Porta ac consectetur ac</button>
+                        <button type="button" class="list-group-item list-group-item-action">Vestibulum at eros</button>
+                        {this.listElements}
+                    </div>
                 </div>
             </div>
         );
