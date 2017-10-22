bc4d5815e4a32444bc9d383b9631e4887d7dde7e
pigi5 <ford.hash@gmail.com>
2017-10-21 20:27:36 -0500

    test home

--- front/src/home.js
+++ front/src/home.js
@@ -2,14 +2,28 @@ import React, { Component } from 'react';
 import Navbar from './navbar';
 
 export default class Home extends Component {
-
     constructor(props) {
         super(props);
     }
 
     render() {
         return (
-            <Navbar />
+            <div>
+                <Navbar />
+                <div className='container'>
+                    <h4>
+                        header
+                    </h4>
+                    <ul>
+                        <li>
+                            bullet 1
+                        </li>
+                        <li>
+                            bullet 2
+                        </li>
+                    </ul>
+                </div>
+            </div>
         );
     }
 }
