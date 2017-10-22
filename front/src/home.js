147e0bea7d1660eecffbdb8861657ba455147c08
EvWilson <everettasherwilson@gmail.com>
2017-10-22 07:30:04 -0500

    DB up

--- front/src/home.js
+++ front/src/home.js
@@ -1,7 +1,23 @@
 import React, { Component } from 'react';
 import Navbar from './navbar';
+import axios from 'axios';
 
 export default class Home extends Component {
+    constructor(props) {
+        super(props);
+
+        axios.get('/mongo')
+        .then(function(response) {
+            console.log(response);
+
+            this.setState({
+                docs: response.data
+            });
+        })
+        .catch(function(error) {
+            console.log(error);
+        });
+    }
     render() {
         return (
             <div className='col'>
