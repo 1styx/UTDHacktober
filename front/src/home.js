ad634e1c43dd389f21c52b2613d612c06e17ee86
EvWilson <everettasherwilson@gmail.com>
2017-10-22 10:18:20 -0500

    Almost there

--- front/src/home.js
+++ front/src/home.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {Redirect} from 'react-router';
 import Navbar from './navbar';
 import axios from 'axios';
 
@@ -8,7 +9,9 @@ export default class Home extends Component {
         super(props);
 
         this.state = {
-            docs: []
+            docs: [],
+            shouldRedirect: false,
+            redirectComponent: null
         }
 
         axios.get('/mongo', {
@@ -30,31 +33,41 @@ export default class Home extends Component {
         });
 
         this.listElement = this.listElement.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick(search) {
+
+        console.log('Clicking!... Search: ' + search);
+        this.state.redirectComponent = <Redirect from={'/'} to={'/search/' + encodeURIComponent(search)} />;
     }
 
     listElement(value, index, array) {
-
+        var outerScope = this;
         return (
-            this.state.docs.map(function(doc){
-                return <button type="button" className="list-group-item list-group-item-action">{doc.search}</button>;
+            this.state.docs.map(function(value, index, array){
+                return <a key={index} className="button list-group-item list-group-item-action" href={'/search/' + encodeURIComponent(value.search)}>{value.search}</a>;
               })
         )
 
     }
 
     render() {
+
         return (
             <div>
+                {this.state.redirectComponent}
                 <Navbar pageName='Home' parentUrl={this.props.match.url} />
                 <div className='container'>
-                    <div className="list-group" style={{float:'right'}}>
+                    <div className="list-group w-25" style={{float:'right'}}>
                         <h3>Recent Items</h3>
                         {this.listElement()}
                     </div>
                     <img src={'https://image.ibb.co/ksvCvm/Mother_Ship.png'}/>
-                    <h2 style={{clear:'left'}}>Welcome to The MotherShip</h2>
+                    <h2>Welcome to The MotherShip</h2>
                 </div>
             </div>
         );
+
     }
 }
