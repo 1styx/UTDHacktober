69cf2b2353eed7b7ececc8432ee0cc527bc69dd5
pigi5 <ford.hash@gmail.com>
2017-10-21 19:52:08 -0500

    finish prototype search page

--- front/src/search.js
+++ front/src/search.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import zip from './util';
+import {zip} from './util';
 import Navbar from './navbar';
 
 export default class SearchResults extends Component {
@@ -7,33 +7,17 @@ export default class SearchResults extends Component {
         super(props);
         // props.searchQuery is the string passed to the backend call
         /*
-        axios.get('/aliProduct', {
+        axios.get('/searchResults', {
                 params: {
                     query: this.props.searchQuery
                 }
             })
             .then(function(response) {
                 console.log(response);
-                this.state.aliResults = response['data'];
-                if (typeof this.state.amazonResults !== 'undefined') {
-                    this.state.zippedResults = zip(this.state.aliResults, this.state.amazonResults);
-                }
-            })
-            .catch(function(error) {
-                console.log(error);
-            }
-        );
-        axios.get('/amazonProduct', {
-                params: {
-                    query: this.props.searchQuery
-                }
-            })
-            .then(function(response) {
-                console.log(response);
-                this.state.amazonResults = response['data'];
-                if (typeof this.state.aliResults !== 'undefined') {
-                    this.state.zippedResults = zip(this.state.aliResults, this.state.amazonResults);
-                }
+                this.state.aliResults = response['data']['alibabaResults'];
+                this.state.amazonResults = response['data']['amazonResults'];
+                this.state.zippedResults = zip(this.state.aliResults, this.state.amazonResults);
+                this.state.analysis = response['data']['analysis'];
             })
             .catch(function(error) {
                 console.log(error);
@@ -42,26 +26,135 @@ export default class SearchResults extends Component {
         */
         this.state = {zippedResults: [[{name: 'aliproduct1'}, {name: 'amaproduct1'}],
             [{name: 'aliproduct2'}, {name: 'amaproduct2'}],
-            [{name: 'aliproduct3'}, {name: 'amaproduct3'}]]};
+            [{name: 'aliproduct3'}, {name: 'amaproduct3'}]],
+            analysis: {
+                query: 'test query',
+                avgProfitMarginRaw: 23.342,
+                avgProfitMarginPercent: 32.0,
+                maxProfitMarginRaw: 23.342,
+                maxProfitMarginPercent: 32.0,
+                avgPriceAlibaba: 23.342,
+                medPriceAlibaba: 23.342,
+                minPriceAlibaba: 23.342,
+                maxPriceAlibaba: 23.342,
+                avgPriceAmazon: 32.0,
+                medPriceAmazon: 32.0,
+                minPriceAmazon: 32.0,
+                maxPriceAmazon: 32.0,
+                ourEval: 'Good'
+            }};
+        this.state.aliResults = [
+            {
+                productInfo: {
+                    name: 'product 1',
+                    price: 30,
+                    url: 'https://google.com',
+                    imgUrl: 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png'
+                },
+                productAnalysis: {
+                    isMin: false,
+                    isMax: false,
+                    profitMarginRaw: 23.345,
+                    profitMarginPercent: 32.32,
+                    ourEval: 'Good'
+                }
+            },
+            {
+                productInfo: {
+                    name: 'product 2',
+                    price: 60,
+                    url: 'https://google.com',
+                    imgUrl: 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png'
+                },
+                productAnalysis: {
+                    isMin: false,
+                    isMax: false,
+                    profitMarginRaw: 23.345,
+                    profitMarginPercent: 32.32,
+                    ourEval: 'Good'
+                }
+            },
+            {
+                productInfo: {
+                    name: 'product 3',
+                    price: 20,
+                    url: 'https://google.com',
+                    imgUrl: 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png'
+                },
+                productAnalysis: {
+                    isMin: false,
+                    isMax: false,
+                    profitMarginRaw: 23.345,
+                    profitMarginPercent: 32.32,
+                    ourEval: 'Good'
+                }
+            }];
+        this.state.amazonResults = [
+            {
+                productInfo: {
+                    name: 'product 4',
+                    price: 30,
+                    url: 'https://google.com',
+                    imgUrl: 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png'
+                },
+                productAnalysis: {
+                    isMin: true,
+                    isMax: false,
+                    profitMarginRaw: 23.345,
+                    profitMarginPercent: 32.32,
+                    ourEval: 'Good'
+                }
+            },
+            {
+                productInfo: {
+                    name: 'product 5',
+                    price: 60,
+                    url: 'https://google.com',
+                    imgUrl: 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png'
+                },
+                productAnalysis: {
+                    isMin: false,
+                    isMax: false,
+                    profitMarginRaw: 23.345,
+                    profitMarginPercent: 32.32,
+                    ourEval: 'Good'
+                }
+            },
+            {
+                productInfo: {
+                    name: 'product 6',
+                    price: 20,
+                    url: 'https://google.com',
+                    imgUrl: 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png'
+                },
+                productAnalysis: {
+                    isMin: false,
+                    isMax: false,
+                    profitMarginRaw: 23.345,
+                    profitMarginPercent: 32.32,
+                    ourEval: 'Good'
+                }
+            }];
+        this.state.zippedResults = zip(this.state.aliResults, this.state.amazonResults);
 
         this.createProductAnalysisCard = this.createProductAnalysisCard.bind(this);
-        this.createProductCard = this.createProductCard.bind(this);
+        this.createProductInfoCard = this.createProductInfoCard.bind(this);
         this.createProductRow = this.createProductRow.bind(this);
     }
 
-    createProductCard(product) {
+    createProductInfoCard(product) {
         if (product == null) {
             return (
-                <div className="card">
+                <div className="card" style={{height : '100%'}}>
                 </div>
             );
         } else {
             return (
-                <div className="card">
+                <div className="card" style={{height : '100%'}}>
                     <div className="card-block">
-                        <h4 className="card-title">{product.name}</h4>
-                        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                        <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+                        <img className="card-img-top img-fluid" src={product.productInfo.imgUrl} alt={product.productInfo.name} />
+                        <h4 className="card-title">{product.productInfo.name}</h4>
+                        <a href={product.productInfo.url}>Go to Product Page</a>
                     </div>
                 </div>
             );
@@ -71,16 +164,20 @@ export default class SearchResults extends Component {
     createProductAnalysisCard(product) {
         if (product == null) {
             return (
-                <div className="card">
+                <div className="card" style={{height : '100%'}}>
                 </div>
             );
         } else {
             return (
-                <div className="card">
+                <div className="card" style={{height : '100%'}}>
                     <div className="card-block">
-                        <h4 className="card-title">{product.name}</h4>
-                        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                        <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+                        <ul>
+                            <li>{'Max: ' + product.productAnalysis.isMax}</li>
+                            <li>{'Min: ' + product.productAnalysis.isMin}</li>
+                            <li>{'Profit Margin $: ' + product.productAnalysis.profitMarginRaw}</li>
+                            <li>{'Profit Margin %: ' + product.productAnalysis.profitMarginPercent}</li>
+                            <li>{'Product Evalution: ' + product.productAnalysis.ourEval}</li>
+                        </ul>
                     </div>
                 </div>
             );
@@ -89,11 +186,19 @@ export default class SearchResults extends Component {
 
     createProductRow(curVal, index, array) {
         return (
-            <div className='row' key={index}>
-                {this.createProductCard(curVal[0])}
-                {this.createProductAnalysisCard(curVal[0])}
-                {this.createProductAnalysisCard(curVal[1])}
-                {this.createProductCard(curVal[1])}
+            <div className="row no-gutters" key={index}>
+                <div className='col-3'>
+                    {this.createProductInfoCard(curVal[0])}
+                </div>
+                <div className='col-3'>
+                    {this.createProductAnalysisCard(curVal[0])}
+                </div>
+                <div className='col-3'>
+                    {this.createProductAnalysisCard(curVal[1])}
+                </div>
+                <div className='col-3'>
+                    {this.createProductInfoCard(curVal[1])}
+                </div>
             </div>
         );
     }
@@ -103,18 +208,37 @@ export default class SearchResults extends Component {
             <div>
                 <Navbar />
                 <div className='container'>
-                    <div className='row'>
-                        <div className="card">
-                            <div className="card-block">
-                                <h4 className="card-title">Product Info</h4>
-                                <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
-                            </div>
+                    <div className="card">
+                        <div className="card-block">
+                            <h4 className="card-title">{'Product Analytics for "' + this.state.analysis.query + '"'}</h4>
+                            <ul className="card-text">
+                                <li>{'Average Profit Margin $: ' + this.state.analysis.avgProfitMarginRaw}</li>
+                                <li>{'Average Profit Margin %: ' + this.state.analysis.avgProfitMarginPercent}</li>
+                                <li>{'Max Profit Margin $: ' + this.state.analysis.maxProfitMarginRaw}</li>
+                                <li>{'Max Profit Margin %: ' + this.state.analysis.maxProfitMarginPercent}</li>
+                                <li>
+                                    Alibaba Analytics
+                                    <ul>
+                                        <li>{'Average Price: ' + this.state.analysis.avgPriceAlibaba}</li>
+                                        <li>{'Median Price: ' + this.state.analysis.medPriceAlibaba}</li>
+                                        <li>{'Min Price: ' + this.state.analysis.minPriceAlibaba}</li>
+                                        <li>{'Max Price: ' + this.state.analysis.maxPriceAlibaba}</li>
+                                    </ul>
+                                </li>
+                                <li>
+                                    Amazon Analytics
+                                    <ul>
+                                        <li>{'Average Price: ' + this.state.analysis.avgPriceAmazon}</li>
+                                        <li>{'Median Price: ' + this.state.analysis.medPriceAmazon}</li>
+                                        <li>{'Min Price: ' + this.state.analysis.minPriceAmazon}</li>
+                                        <li>{'Max Price: ' + this.state.analysis.maxPriceAmazon}</li>
+                                    </ul>
+                                </li>
+                                <li>{'Overall Evaluation: ' + this.state.analysis.ourEval}</li>
+                            </ul>
                         </div>
                     </div>
-                    <div className="card-deck">
-                        {this.state.zippedResults.map(this.createProductRow)}
-                    </div>
+                    {this.state.zippedResults.map(this.createProductRow)}
                 </div>
             </div>
         );
